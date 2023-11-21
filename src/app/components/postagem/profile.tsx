cb4115f7065525627bc768a4faa337e28fc2dfc3
Emesson Silva <140443316+emessonSilva@users.noreply.github.com>
2023-11-21 16:52:52 -0300

    Update profile.tsx
    liked button added

--- src/app/components/postagem/profile.tsx
+++ src/app/components/postagem/profile.tsx
@@ -1,6 +1,7 @@
+import React, { useState } from 'react';
+
 interface ProfileProps {
   username: string;
-  // status: string;
   foto: any;
   tamanhofoto: number;
   comprimentopostagem: number;
@@ -8,35 +9,60 @@ interface ProfileProps {
   cor: string;
   borda: number;
   postagem: any;
-  likedButton: any;
 }
 
 export function ProfileUser(props: ProfileProps) {
+  
+  //hook useState
+  const [filledHeart, setfilledHeart] = useState(false);
+
+  const handleFilledHeart = () => {
+    setfilledHeart(!filledHeart);
+  };
+
   return (
     <>
-    <div>
-      <div style={{ display: "flex", alignItems: "center", position: "absolute", top: 10 }}>
-        <img
-          style={{ borderRadius: props.borda}}
-          src={props.foto}
-          alt={props.username}
-          width={props.tamanhofoto}
-        />
-        <h5 style={{ color: props.cor, marginLeft: 10 }}>Username: {props.username}</h5>
-      </div>
-      {/* <p style={{ color: props.cor }}>Status: {props.status}</p> */}
       <div>
-        <img 
-        style={{marginTop: 50}}
-        src={props.postagem} 
-        width={props.comprimentopostagem} 
-        height={props.alturapostagem}/>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            position: "absolute",
+            top: 10,
+          }}
+        >
+          <img
+            style={{ borderRadius: props.borda }}
+            src={props.foto}
+            alt={props.username}
+            width={props.tamanhofoto}
+          />
+          <h5 style={{ color: props.cor, marginLeft: 10 }}>
+            Username: {props.username}
+          </h5>
+        </div>
+        <div>
+          <img
+            style={{ marginTop: 50 }}
+            src={props.postagem}
+            width={props.comprimentopostagem}
+            height={props.alturapostagem}
+          />
+        </div>
+        <div>
+          <i
+          //const heartClassName = filledHeart ? 'fa fa-heart' : 'fa fa-heart-o'; o debaixo é a msm coisa
+            className={`fa ${filledHeart ? 'fa-heart' : 'fa-heart-o'}`}
+            style={{
+              color: filledHeart ? 'red' : 'black',
+              fontSize: 20,
+              marginTop: 5,
+              cursor: 'pointer',
+            }}
+            onClick={handleFilledHeart}
+          ></i>
+        </div>
       </div>
-      <div>
-      <i className="fa fa-heart-o"></i>
-      </div>
-    </div>
     </>
   );
 }
-
